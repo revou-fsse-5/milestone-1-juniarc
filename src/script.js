aee2fb376ef8082b5e545a309df7fb29a93c19d1
Cahya Juniar <juniarcahya@gmail.com>
2024-07-12 10:00:37 +0700

    add dark-mode

--- src/script.js
+++ src/script.js
@@ -1,3 +1,6 @@
+const IS_DARK_MODE = 'isDarkMode';
+const DARK_THEME_CLASS = 'dark-mode'
+
 const heroFirstAnimation = document.querySelector('.section__hero__text-container__text-top')
 const heroSecondAnimation = document.querySelectorAll('.section__hero__type');
 const heroThirdAnimation = document.querySelector('.section__hero__bg-img');
@@ -46,4 +49,59 @@ burgerBtn.addEventListener('click', () => {
 closeBtn.addEventListener('click', () => {
     navMenu.classList.toggle('open')
 })
+
+// Mode Configuration
+
+// Check avaibility localStorage
+function isStorageExist() {
+    if (typeof (Storage) === undefined ){
+        alert('Your browser did not support local storage');
+        return false;
+    }
+    return true;
+}
+const modeBtnDark = document.querySelector('.header__mode-btn__dark')
+const modeBtnLight = document.querySelector('.header__mode-btn__light')
+const moonIcon = document.querySelector('.mode-btn__moon-icon');
+const sunIcon = document.querySelector('.mode-btn__sun-icon');
+
+modeBtnDark.addEventListener('click', () => {
+    moonIcon.classList.add('animation__mode-btn')
+    if(!document.body.classList.contains(DARK_THEME_CLASS)) {
+        document.body.classList.add(DARK_THEME_CLASS);
+        localStorage.setItem(IS_DARK_MODE, true);
+    }
+    modeBtnDark.classList.toggle('hide');
+    moonIcon.classList.remove('animation__pop-up-icon')
+    moonIcon.classList.remove('animation__mode-btn');
+    sunIcon.classList.add('animation__pop-up-icon')
+    modeBtnLight.classList.toggle('hide')
+})
+
+modeBtnLight.addEventListener('click', () => {
+    sunIcon.classList.add('animation__mode-btn')
+    if(document.body.classList.contains(DARK_THEME_CLASS)) {
+        document.body.classList.remove(DARK_THEME_CLASS);
+        localStorage.setItem(IS_DARK_MODE, false);
+    }
+    modeBtnLight.classList.toggle('hide')
+    sunIcon.classList.remove('animation__pop-up-icon')
+    sunIcon.classList.remove('animation__mode-btn');
+    moonIcon.classList.add('animation__pop-up-icon')
+    modeBtnDark.classList.toggle('hide')
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    if(isStorageExist()) {
+        const getModeFromLocalStorage = localStorage.getItem(IS_DARK_MODE);
+        if(getModeFromLocalStorage === 'true') {
+            document.body.classList.add(DARK_THEME_CLASS)
+            modeBtnDark.classList.toggle('hide')
+        } else {
+            modeBtnLight.classList.toggle('hide')
+        }
+    }
+})
+
+
 
